112a761c75889bc47aab17759288cffb1c411bb0
wwwtravel <root@rails.(none)>
2011-04-26 02:05:11 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myticket::Application.config.secret_token = '25d599f778e672de7e3e9408ff7e8b42de77e5f4fe453ff66cc8045f4e5b454ef54a6a2224719570c80cb2809e45e9cc5d1168936dbaa60938ff64d43e4282c1'
